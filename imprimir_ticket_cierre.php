764ed004acae2567c6571b6170095e9f600824ec
Terry Ramirez <terryman42@gmail.com>
2025-10-14 18:44:07 -0600

    PRE FACTURA username empleado

--- imprimir_ticket_cierre.php
+++ imprimir_ticket_cierre.php
@@ -64,7 +64,7 @@ $efectivoEntregar = $apertura + $ingresos + $totalesPago['Efectivo'] - $egresos;
 // Datos para el ticket
 $configModel = new ConfigModel();
 $restaurante = $configModel->get('nombre_app') ?: 'Restaurante';
-$empleado = isset($_SESSION['username']) ? $_SESSION['username'] : 'Usuario';
+$empleado = (isset($_SESSION['username']) ? $_SESSION['username'] : 'Desconocido');
 $ticketId = date('Ymd') . rand(100,999);
 $moneda = $configModel->get('moneda') ?: 'C$';
 
